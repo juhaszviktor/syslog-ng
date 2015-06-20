9ed117c6ac31fa15764ca1ecf548342706728cf1
Peter Szabo <asdwsda@gmail.com>
2015-06-20 19:06:47 +0200

    java-filter: fix copyright notice

--- modules/java/proxies/java-filter-proxy.c
+++ modules/java/proxies/java-filter-proxy.c
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2010-2014 BalaBit IT Ltd, Budapest, Hungary
- * Copyright (c) 2010-2014 Viktor Juhasz <viktor.juhasz@balabit.com>
+ * Copyright (c) 2015 BalaBit IT Ltd, Budapest, Hungary
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published
@@ -20,7 +19,6 @@
  * COPYING for details.
  *
  */
-
 
 #include "java-filter-proxy.h"
 #include "java-logmsg-proxy.h"
